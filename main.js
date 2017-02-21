5e81d2ca4a5547749fe4110b8a07058d4fd6ebc4
Justin Samuels <blizm87@gmail.com>
2017-02-21 10:15:56 -0800

    completed code challenge

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-main.js
+// main.js
+console.log('Hello Justin');
+const firBtn = document.createElement('button');
+const firInput = document.createElement('input');
+const enterBtn = document.createElement('button');
+const nestRootDiv = document.createElement('div');
+const rootDiv = document.querySelector('#root');
+const body = document.querySelector('body');
+let num = 0;
+
+rootDiv.append(firBtn);
+rootDiv.append(firInput);
+rootDiv.append(enterBtn);
+rootDiv.append(nestRootDiv);
+
+firBtn.textContent = num;
+enterBtn.textContent = 'Submit Color';
+firBtn.addEventListener('click', function (evt){
+  num++;
+  firBtn.textContent = num;
+});
+
+  let rendHtml = [];
+enterBtn.addEventListener('click', function(evt){
+  body["style"] = "background-color: " + firInput.value + ";";
+  rendHtml.push(firInput.value);
+  let compHtml = '<ol>';
+  rendHtml.forEach(function(newColor){
+    compHtml += '<li>' + newColor + '</li>';
+  })
+  compHtml += '</ol>';
+  nestRootDiv.innerHTML = compHtml;
+});
+
+
